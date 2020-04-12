e8c14c2c0b1ea6942130937daf299d4a4d9762fb
Shuangqiu Zheng <qiu_zs@126.com>
2020-04-12 12:16:28 +0800

    init repo

---
+++ src/test/java/cn/qiudev/unittest/UnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.qiudev.unittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
